333af2168c93181fa925056d6c2fa2f9a09357f7
Steve Yoo <s.yoo@avanade.com>
2017-12-14 12:28:20 -0600

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, private socialSharing: SocialSharing) {
 
-  }
+	}
 
+	sendEmail() {
+		this.socialSharing.shareViaEmail('test body from ionic', 'test from ionic', ['stevecyoo@gmail.com']).then(() => {
+			// Success!
+		}).catch(() => {
+			// Error!
+		});
+	}
 }
